66da76bd3d5eaac95d8cc22ba63f23a9a32b3a20
AMRESH-SLACK <67509274+AMRESH-SLACK@users.noreply.github.com>
2020-09-10 10:00:48 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+function rainy() {
+    document.body.style.backgroundImage = "url('./image/038adf51bb940a003168b0e243ecc69a.gif')";
+    document.body.style.backgroundSize= "100%";
+    document.body.style.backgroundRepeat= "no-repeat";
+    document.getElementById("rainy").style.display  = "none";
+    document.getElementById("rain").style.display = "inherit";
+    document.getElementById("hot").style.display = "none";
+    document.getElementById("para").style.display = "inherit";
+    document.getElementById("summers").style.display = "none";
+}
+
